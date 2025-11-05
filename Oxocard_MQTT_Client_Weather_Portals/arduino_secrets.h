240d9c7fe89220ad194de2940ee089ae4793a51b
Make Magazin <mail@make-magazin.de>
2025-11-05 02:01:13 +0100

    Add files via upload

---
+++ Oxocard_MQTT_Client_Weather_Portals/arduino_secrets.h
@@ -0,0 +1,25 @@
+// This file contains credentials to access several services
+
+// WiFi
+#define SECRET_SSID   "Sunrise_2.4GHz_19B4C2"
+#define SECRET_PASS   "u2u7xxxxx1Ds"
+
+
+// TTN MQTT 
+const char broker[] = "eu1.cloud.thethings.network";
+int        port     = 1883;
+const char topic[]  = "v3/cksensorhub@ttn/devices/new-sensorhub/up";
+
+// WeatherCloud
+// Weathercloud ID: 3b5432ecf90584dc
+// Key: 4e41becbcb2fc08aa69ca5944ebe9ea0
+// softwareId: 74e0224507d0.
+#define WID "3b5xxxxxxx584dc"
+#define KEY "4e41becbcb2fc08aa69ca5944ebe9ea0"
+#define SWID "74e0224507d0"
+
+// Meteologix
+// Station ID: 24
+// Station Key: gK53MhcMk9eeU2NY6JJMnWAXaQ7trKmC
+#define STATION_ID_NUMBER 24
+#define STATION_KEY "gK53MhcxxxxxxxNY6JJMnWAXaQ7trKmC"
